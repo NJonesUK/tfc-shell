2b3b73e2fc10115bccfba35678f5ecfe44fb91ff
Nick Jones <nick.jones@f-secure.com>
2022-01-18 12:45:00 +0100

    more recon

--- recon.sh
+++ recon.sh
@@ -1,13 +1,14 @@
 chmod +x ./jq
 ./jq --null-input \
-    --arg whoami "`whoami`" \
+    --arg id "`id`" \
     --arg printenv "`printenv`" \
     --arg os "`cat /etc/issue`" \
     --arg hostname "`hostname`" \
     --arg processes "`ps -aux`" \
     --arg mounts "`mount`" \
     --arg tools "`which awk perl python ruby gcc cc vi vim nmap find netcat nc wget tftp ftp 2>/dev/null`" \
-    '{"whoami": $whoami, "env": $printenv, "os": $os, "hostname": $hostname, "processes": $processes, "mounts": $mounts, "tools": $tools}'
+    --arg curl-google "`curl google.com`"
+    '{"id": $id, "env": $printenv, "os": $os, "hostname": $hostname, "processes": $processes, "mounts": $mounts, "tools": $tools, "curl-google": $curlgoogle}'
 
 
 # printf '{"whoami": "%q", "env": "%q"}' "`whoami`"  "`printenv`"
